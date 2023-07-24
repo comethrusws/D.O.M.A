07e95fd58e7aa937613cc27f15e8e2db532a342c
chillobae <basabjha@naver.com>
2023-07-24 12:49:59 +0545

    Commit #5: Integrated tensorflow

--- main.py
+++ main.py
@@ -1,99 +1,92 @@
+#D.O.M.A : Deep-learning Oriented Multilingual Assistant
+
 import random
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-from PyPDF2 import PdfReader
-import glob
 from langdetect import detect
 from googletrans import Translator
-from googlesearch import search
 import time
+import googlesearch
+import threading
+import transformers
+import tensorflow as tf
 
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
 
-# predefined patterns and responses
+t5_model = transformers.TFT5ForConditionalGeneration.from_pretrained('t5-small')
+t5_tokenizer = transformers.T5Tokenizer.from_pretrained('t5-small')
+
+#patterns ra responses(predefined)
 patterns = [
-    (["hello", "hi", "hey"], ["Hi!", "Hello!", "Hey!"]),
-    (["how", "are", "you"], ["I'm good, thanks!", "I'm doing great!", "I'm fine."]),
-    (["name"], ["I'm a chatbot. My Name is DOMA!", "You can call me DOMA!", "I don't have a name."]),
-    (["what","is","your","name"], ["I'm a chatbot. My Name is DOMA!", "You can call me DOMA!", "I don't have a name."]),
+    (["hello", "hi", "hey"], ["Hi there!", "Hello!", "Hey! What is up?"]),
+    (["how", "are", "you"], ["I'm good, thanks!", "I'm doing great!", "I'm fine.How are you?"]),
+    (["name"], ["I am DOMA. Nice to meet you!", "You can call me DOMA!"]),
+    (["what", "is", "your", "name"], ["My Name is DOMA! I'm a chatbot. ", "You can call me DOMA!","Hi! I a DOMA!"]),
     (["bye", "goodbye"], ["Goodbye!", "See you later!", "Take care!"]),
     (["how", "old", "you"], ["I am just a computer program, so I don't have an age."]),
-    (["what", "your", "purpose"], ["I am here to assist and answer your questions."]),
-    (["who", "created", "you"], ["I was created by a Developer named Basab."]),
-    (["tell", "joke"], ["Why don't scientists trust atoms? Because they make up everything!"]),
+    (["what", "your", "you","do","can","ability","purpose"], ["I am here to assist and answer your questions."]),
+    (["who", "created", "made", "you"], ["I was created by a Developer named Basab."]),
+    (["tell", "me", "jokes" ,"joke"], ["Why don't scientists trust atoms? Because they make up everything!⚛","Why was 6 aftrai of 7? Because 7,8,9!😂" "What do you call a south asian electrician? A-Shok(Get it?)🤯⚡","what happens to a ilegally parked frog? It gets Toad away.🐸"]),
 ]
 
 stop_words = set(stopwords.words('english'))
 lemmatizer = WordNetLemmatizer()
 
-# user input preprocessing
+# user input preprocess
 def preprocess_input(user_input):
     tokens = word_tokenize(user_input.lower())
     filtered_tokens = [lemmatizer.lemmatize(token) for token in tokens if token.isalnum() and token not in stop_words]
     return filtered_tokens
 
-
-def extract_text_from_pdf(file_path):
-    try:
-        with open(file_path, 'rb') as file:
-            pdf = PdfReader(file)
-            text = ''
-            for page in pdf.pages:
-                text += page.extract_text()
-            return text
-    except Exception as e:
-        print(f"Error while extracting text from PDF: {str(e)}")
-        return ''
-
-# load and preprocess pdf
-def load_pdfs():
-    pdf_files = glob.glob("C:/Users/Basab/Desktop/D.O.M.A/Database/*.pdf")
-    pdf_texts = []  # Initialize the list to store text from PDFs
-    for pdf_file in pdf_files:
-        file_path = pdf_file
-        text = extract_text_from_pdf(pdf_file)
-        pdf_texts.append(text)
-    return pdf_texts
-
-def generate_response(user_input, pdf_texts):
+def generate_response(user_input):
+    # Preprocess the user input
     preprocessed_input = preprocess_input(user_input)
     for pattern, responses in patterns:
-        if all(word in preprocessed_input for word in pattern):
+        if any(word in preprocessed_input for word in pattern):
             return random.choice(responses)
-
-    combined_text = ' '.join(pdf_texts)
 
     detected_lang = detect(user_input)
 
     if detected_lang != 'en':
         translator = Translator()
         try:
-            user_input = translator.translate(user_input, src=detected_lang, dest='en').text
-        except (AttributeError, ConnectionError) as e:
-            print(f"Google Translate API error: {e}")
-            return "Oops! Something went wrong with translation. Please try again later."
+            translated_text = translator.translate(user_input, src=detected_lang, dest='en')
+            user_input = translated_text.text
+        except Exception as e:
+            print(f"Translation error: {e}")
+            return "Oops! Something went wrong with translation. Can you repeat that?"
         time.sleep(1)
 
-    # Web search
-    search_results = list(search(user_input, num=3, stop=3))
-    if search_results:
-        return f"Here are some search results for '{user_input}':\n" + "\n".join(search_results)
+    if "search" in preprocessed_input:
+        search_query = user_input.replace("search", "").strip()
+        try:
+            # google search perform garney
+            search_results = googlesearch.search(search_query, num_results=3)
+            if search_results:
+                return "\n".join(search_results)
+            else:
+                return "Sorry, I couldn't find any relevant results for your search."
+        except Exception as e:
+            print(f"Google search error: {e}")
+            return "Oops! Something went wrong with the search. Please try again later."
 
-    return "DOMA: I'm sorry, I don't understand that. Can you please rephrase?"
+    # If no predefined patterns match and not a search query, T5 model for response generation
+    inputs = t5_tokenizer("DOMA: " + user_input, return_tensors="tf", max_length=512, padding="max_length", truncation=True)
+    outputs = t5_model.generate(inputs.input_ids, max_length=100, num_return_sequences=1, early_stopping=True)
+    response = t5_tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-pdf_texts = load_pdfs()
+    return response
 
 def main():
-    pdf_texts = load_pdfs()  # Load and preprocess the pdfs
     while True:
         user_input = input("User: ")
-        response = generate_response(user_input, pdf_texts)
+        response = generate_response(user_input)
         if not response:
-            # Basic conversation responses
+            # Basic conversation response haru
             if "thank you" in user_input.lower():
                 print("DOMA: You're welcome!")
             elif "how are you" in user_input.lower():
@@ -107,20 +100,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# Main loop
-while True:
-    user_input = input("User: ")
-    response = generate_response(user_input, pdf_texts)
-    if not response:
-        # Basic conversation responses
-        if "thank you" in user_input.lower():
-            print("DOMA: You're welcome!")
-        elif "how are you" in user_input.lower():
-            print("DOMA: I'm doing well, thank you!")
-        elif "hello" in user_input.lower():
-            print("DOMA: Hi! How are you doing today?")
-        else:
-            print("DOMA: I'm sorry, I don't understand that. Can you please rephrase?")
-    else:
-        print("DOMA:", response)
